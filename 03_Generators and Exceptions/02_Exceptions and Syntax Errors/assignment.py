755cbf0f45dd9880cdf62d6491dc2e0eac9d60d0
Your Name <you@example.com>
2024-01-24 07:59:28 -0500

    u3 l2

---
+++ 03_Generators and Exceptions/02_Exceptions and Syntax Errors/assignment.py
@@ -0,0 +1,6 @@
+try:
+    value = int(input("Enter a number: "))
+    value2 = int(input("Enter another number: "))
+    print(value / value2)
+except ZeroDivisionError:
+    print("Division by zero is not ossible, sorry.")
