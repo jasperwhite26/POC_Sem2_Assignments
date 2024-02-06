d1ba8db12a8fbf50f316cc04e24593cc4b2a5344
Your Name <you@example.com>
2024-02-06 08:20:00 -0500

    u6 l1

--- 06_Types, Modules, and Packages/01_Type Annotations/assignment.py
+++ 06_Types, Modules, and Packages/01_Type Annotations/assignment.py
@@ -1,9 +1,6 @@
-#add type annotation on the functions and variables
-#variables types are float, function return type is a float
-
-def average(a, b):
+def average(a, b) -> float:
     return (a + b) / 2
 
-number1 = float(input("Enter a decimal number: "))
-number2 = float(input("Enter another decimal number: "))
+number1: float = float(input("Enter a decimal number: "))
+number2: float = float(input("Enter another decimal number: "))
 print(average(number1, number2))
