60faeee122c9a2ee1fd09cb934f80ad8fa961384
Your Name <you@example.com>
2024-02-07 07:57:03 -0500

    u5 l1

--- 05_More on Exceptions/01_Else and Finally/assignment.py
+++ 05_More on Exceptions/01_Else and Finally/assignment.py
@@ -1,22 +1,12 @@
-#Continue with code from 3.3
-
 number1 = 0
 number2 = 0
 try:
-    number1 = int(input("Enter a number"))
-    # YOUDO.  use input function and int to set number2
-except ValueError:
-    print("An input was not correct")
+    number1 = int(input("Enter a number: "))
+    number2 = int(input("Enter another number: "))
+    print(number1 / number2)
+except ZeroDivisionError:
+    print("Division by zero is not possible, sorry")
 else:
     print("No value error detected")
 finally:
-    print("Values taken care of")
-
-try:
-    # YOUDO divide number1 / number2 and set to answer
-    # YOUDO  print the result of the division (aka answer with some helper text)
-    pass  # YOUDO remove pass when done
-except ZeroDivisionError:
-    # YOUDO:  print message stating that division by zero is not possible.
-    pass  # YOUDO remove pass when done
-#YOUDO:  else and finally here as well.  
+    print("I always run")
