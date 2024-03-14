a2e3833706b8f61c5b134600ea590e5fe0bc7467
Your Name <you@example.com>
2024-03-14 08:36:23 -0400

    u8 l5

--- 08_OOP/04_Class and Instance Variables/assignment.py
+++ 08_OOP/04_Class and Instance Variables/assignment.py
@@ -1,7 +1,5 @@
 class Rectangle:
     
-    
-    
     def __init__(self, base: float, height: float) -> None:
         if(base < 0):
             self.__base = 0
@@ -16,14 +14,26 @@ class Rectangle:
     def get_height(self) -> float:
         return self.__height
     
-    #YOUDO the get_base method
+    def get_base(self) -> float:
+        return self.__base
     
     def get_perimeter(self) -> float:
         return 2 * self.__base + 2 * self.__height
     
-    #Youdo get_area method
+    def get_area(self) -> float:
+        return self.__base * self.__height
+    
+
  
- 
- 
-#YOUDO>  create two rectangles.  print their base, height, perimeter, and area
-#using only the methods not the fields/property/attributes
+rect1 = Rectangle(3, 4)
+print("The base of rectangle1 is", rect1.get_base())
+print("The height of rectangle1 is", rect1.get_height())
+print("The perimeter of rectangle1 is", rect1.get_perimeter())
+print("The area of rectangle1 is", rect1.get_area())
+
+rect2 = Rectangle(10, 22)
+print("The base of rectangle2 is", rect2.get_base())
+print("The height of rectangle2 is", rect2.get_height())
+print("The perimeter of rectangle2 is", rect2.get_perimeter())
+print("The area of rectangle2 is", rect2.get_area())
+
