730d2fd2b0c2057c4aff50240c8d7eb98a83cce0
Your Name <you@example.com>
2024-05-01 08:43:49 -0400

    u10 l3

--- 10_Functional Programming/03_File Handling/assignment.py
+++ 10_Functional Programming/03_File Handling/assignment.py
@@ -1,10 +1,8 @@
-file_path = "C:\\Users\\Nicholas Armstrong\\Development\\POC_Sem2_Assignments\\10_Functional Programming\\03_File Handling\\assignment.py"
+file_path = "C:\\Users\\whiteana001\\Dev\\POC_Sem2_Assignments\\10_Functional Programming\\03_File Handling\\myFile.txt"
 
 try:
-    stream = open(file_path)
-    #YOUDO print to console using stream.read()
-    #YOUDO don't forget to close the stream
+    stream = open(file_path, "r")
+    print(stream.read())
+    stream.close
 except:
-    pass
-##YOUDO rest of the program for reading and printing to the console
-##remember pass in file_path to open 
+    print("wrong")
